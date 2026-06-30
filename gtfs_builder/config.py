06671624696c0138d2047e01335af3fc8063cd31
Arq Eduardo Tovar Gómez <eduardo.tovar10@unach.mx>
2026-06-30 14:41:03 -0600

    feat(config): add centralized project configuration

---
+++ gtfs_builder/config.py
@@ -0,0 +1,32 @@
+"""
+Configuración global del proyecto
+UrbanTransit GTFS Builder MX
+"""
+
+# ==========================
+# Rutas
+# ==========================
+
+INPUT_KML = "data/kml/RutaA Tuxtla.kml"
+OUTPUT_FOLDER = "output"
+
+# ==========================
+# Agencia
+# ==========================
+
+AGENCY_ID = 1
+AGENCY_NAME = "UrbanTransit MX"
+AGENCY_URL = "https://github.com/EduardoTovarGomez/urbantransit-gtfs-builder-mx"
+
+# ==========================
+# GTFS
+# ==========================
+
+TIMEZONE = "America/Mexico_City"
+LANGUAGE = "es"
+SERVICE_ID = "WEEKDAY"
+
+START_DATE = "20260101"
+END_DATE = "20261231"
+
+FEED_VERSION = "0.6.0-dev"
